575719fe17bce5c6518cad9c1797affa849faf18
valarmathi <valarkannan@gmail.com>
2022-09-22 13:27:53 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,56 @@
+provider "vsphere" {
+  user           =*****************************
+  password       =*****************************
+  vsphere_server = "10.198.1.13"
+
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
+}
+resource "random_id" "server" {
+  byte_length = 8
+}
+
+data "vsphere_datacenter" "dc" {
+  name = "devcloud"
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = "vmstore"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_resource_pool" "pool" {
+  name          = "fenrir/Resources"
+ datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = "ubuntu16"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_network" "network" {
+  name          = "portGroup-1004"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = "test-checking-test-vm-code${random_id.server.hex}"
+  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+
+  num_cpus = 1
+  memory   = 512
+  guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
+
+  network_interface {
+    network_id = "${data.vsphere_network.network.id}"
+  }
+ 
+wait_for_guest_net_timeout = 0
+
+  disk {
+    label = "disk0"
+    size  = 4
+  }
+}
